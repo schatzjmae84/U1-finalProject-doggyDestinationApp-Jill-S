5f4c13939c9deacf5ddece6de738232cb594ea3b
schatzjmae84 <schatzjm84@gmail.com>
2025-05-21 15:28:04 -0500

    Grid complete

--- doggy-destination-app/src/components/DestinationList.jsx
+++ doggy-destination-app/src/components/DestinationList.jsx
@@ -12,17 +12,21 @@ export default function DestinationList() {
 
     return (
         
-        <div className="container">
+        <div>
             <h2>{pupPlaces}</h2>
-            <div>Link1</div>
-            <div>Link2</div>
-            <div>Link3</div>
-            <div>Link4</div>
-            <div>Link5</div>
-            <div>Link6</div>
-            <div><img className="image" src={image} width="200" height="225" alt="Dog in car" /></div>
-            <div><img className="picture" src={picture} width="300" height="225" alt="Happy Dog Play at the Park" /></div>              
-            <div><Link to="/searchForm">Return to Search Form</Link></div>
+            <div className="container">
+            <div className="item1">Here are some places you can go with your pup based on your search!</div>
+            <div className="item2">Link2</div>
+            <div className="item3">Link3</div>
+            <div className="item4">Link4</div>
+            <div className="item5">Link5</div>
+            <div className="item6">Link6</div>
+            <div className="item7">Link7</div>
+            </div>
+            <div><img className="image" src={image} width="200" height="225" alt="Dog in car" />
+            <img className="picture" src={picture} width="300" height="225" alt="Happy Dog Play at the Park" /><br />              
+            <Link to="/searchForm">Return to Search Form</Link>
+            </div>
         </div>
     );
 
