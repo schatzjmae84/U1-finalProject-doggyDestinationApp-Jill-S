e01fb6d452832adeb36275e733fd360db31f4a51
schatzjmae84 <schatzjm84@gmail.com>
2025-05-28 16:15:13 -0500

    Switching back to main for new idea

--- doggy-destination-app/src/components/SelectedDestination.jsx
+++ doggy-destination-app/src/components/SelectedDestination.jsx
@@ -5,19 +5,20 @@ import dogPic from "../assets/pupPic4.jpg";
 import pupData from "../assets/data.json"
 import { useState, useEffect } from "react";
 import RiseLoader from 'react-spinners/RiseLoader';
-import { filteredDestination } from "../shared/utils";
 
 const SelectedDestination = () => {
 
     const {idealInfo} = useParams();
 
-    /*const outdoorActivities = pupData.filter((dest) => {
+    function handleChange() {
+        pupData.filter((dest) => {
         return dest.activity === "Outdoor";
     });
+};
 
     const socialActivities = pupData.filter((place) => {
         return place.activity === "Social";
-    });*/
+    });
 
     const [ activityType, setActivityType ] = useState("");
     const [ error, setError ] = useState("");
@@ -34,7 +35,6 @@ const SelectedDestination = () => {
     useEffect(() => {
         setLoading(true)
         setTimeout(() => {
-            setDestination(filteredDestination(value));
             setLoading(false)
         }, 5000);
     }, []);
@@ -50,7 +50,7 @@ const SelectedDestination = () => {
                 Select the type of activity you are interested in:
                 <select value={activityType} onChange={(event) => setActivityType(event.target.value)}>
                     <option value="">--Doggy Destinations--</option>
-                    <option value="Outdoor">Outdoor Adventures</option>
+                    <option value="Outdoor" onChange={handleChange}>Outdoor Adventures</option>
                     <option value="Social">Social Settings for both of you!</option>
                 </select> 
             </label>
@@ -60,15 +60,13 @@ const SelectedDestination = () => {
             </div>
             {error && <p style={{color: "red"}}>{error}</p>} 
 
-            {destination.length > 0 && (            
+                        
                 <div>
                     <ul>
-                        {destination.map((place) => {
-                            <li key={place.activity}>{destination}</li>
-                        })}                        
+                                              
                     </ul>
                 </div>
-            )}         
+                     
             <div>
             <img className="image" src={image} width="200" height="225" alt="Dog in car" />
             <img className="picture" src={picture} width="300" height="225" alt="Happy Dog Play at the Park" />
