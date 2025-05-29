21e431e06dd65758f45dd71afe38f9c5ccf66fbb
schatzjmae84 <schatzjm84@gmail.com>
2025-05-29 14:22:37 -0500

    Trying to deploy again

--- doggy-destination-app/src/components/SelectedDestination.jsx
+++ doggy-destination-app/src/components/SelectedDestination.jsx
@@ -42,7 +42,7 @@ const SelectedDestination = () => {
                 Select the type of activity you are interested in:
                 <select value={activityType} onChange={(event) => setActivityType(event.target.value)}>
                     <option value="">--Doggy Destinations--</option>
-                    <option value="Outdoor" onChange={handleChange}>Outdoor Adventures</option>
+                    <option value="Outdoor">Outdoor Adventures</option>
                     <option value="Social">Social Settings for both of you!</option>
                 </select> 
             </label>
@@ -52,7 +52,7 @@ const SelectedDestination = () => {
             </div>
             {error && <p style={{color: "red"}}>{error}</p>} 
 
-            {destination.length > 0 && (                                              
+            {destination.length > 0 && (                                               
                 <div>
                     <h2>Here are the Doggy Destinations in the category you selected:</h2>
                     <ul>
